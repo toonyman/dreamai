252d0f8af3e304046b5e815e879bf8a613d2f975
Younsuk_Kim <toonyman2000@gmail.com>
2026-02-02 14:16:35 +0900

    Add Google Analytics tracking (G-4FBMT32C7V)

--- app/layout.tsx
+++ app/layout.tsx
@@ -86,6 +86,24 @@ export default function RootLayout({
     return (
         <html lang="en">
             <head>
+                {/* Google Analytics */}
+                <Script
+                    strategy="afterInteractive"
+                    src="https://www.googletagmanager.com/gtag/js?id=G-4FBMT32C7V"
+                />
+                <Script
+                    id="google-analytics"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', 'G-4FBMT32C7V');
+                        `,
+                    }}
+                />
+
                 {/* JSON-LD Structured Data for SEO */}
                 <Script
                     id="structured-data"
